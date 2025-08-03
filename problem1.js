83091e45930d9611bb0806c1f44974b30b2d0f74
jowal-islam <ranabrindabon@gmail.com>
2025-08-03 23:36:13 +0600

    4th problem solved

--- problem1.js
+++ problem1.js
@@ -1,31 +1,40 @@
 
-// problem -01 (Divide the Asset)
-var area = 800;
-var perArea = area /2;
-console.log(perArea);
+// // problem -01 (Divide the Asset)
+// var area = 800;
+// var perArea = area /2;
+// console.log(perArea);
 
-// problem - 02 (Cycle or laptop)
-var money = 9999;
+// // problem - 02 (Cycle or laptop)
+// var money = 9999;
 
-if(money >= 25000){
-    console.log('Laptop');
-}else if(money >= 10000){
-    console.log('Cycle');
-}else {
-    console.log('Chocolate');
-}
+// if(money >= 25000){
+//     console.log('Laptop');
+// }else if(money >= 10000){
+//     console.log('Cycle');
+// }else {
+//     console.log('Chocolate');
+// }
 
-// problem -03 (Medicine planner)
-var LastDay = 30;
+// // problem -03 (Medicine planner)
+// var LastDay = 11;
 
-for(var day = 1; day <= LastDay; day++){
-   if(day % 3 === 0){
-     console.log( day , '- medicine');
-   }else{
-     console.log(day,'- rest')
-   }
-}
+// for(var day = 1; day <= LastDay; day++){
+//    if(day % 3 === 0){
+//      console.log( day , '- medicine');
+//    }else{
+//      console.log(day,'- rest')
+//    }
+// }
 
 // problem 04 (Delete / Store)
 
 var fileName = "pdfData.jpg";
+
+if(fileName.includes("pdf")||
+   fileName.includes("docx")||
+   fileName.includes("#")
+){
+    console.log('Store');
+}else{
+    console.log('Delete')
+}
